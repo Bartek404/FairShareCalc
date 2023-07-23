983b4bb43d3b31e6458e9714e64b21975baf9fed
Bartek <110336189+Bartek404@users.noreply.github.com>
2023-07-23 15:54:49 +0200

    changed functions from objects to arrays and simplified some code

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,13 @@ const personTwoIncome = document.querySelector('.person2-earnings')
 const expensesInput = document.querySelector('.expenses')
 const btn = document.querySelector('.btn')
 const error = document.querySelector('.main__error-msg')
-
 const personOneName = document.querySelector('.result-person-one')
 const personOneResult = document.querySelector('.result-one')
 const personTwoName = document.querySelector('.result-person-two')
 const personTwoResult = document.querySelector('.result-two')
 const result = document.querySelector('.result-percentage')
-
-let person1object = {}
-let person2object = {}
+let personOneArray = []
+let personTwoArray = []
 
 const getNames = () => {
 	if (personOneNameInput.value && personTwoNameInput.value !== '') {
@@ -25,20 +23,10 @@ const getNames = () => {
 	}
 }
 
-const getPercentage1 = income => {
-	if (personOneIncome.value != 0) {
+const getPercentage = (income, personArray) => {
+	if (income != 0) {
 		for (let i = 0; i <= 100; i++) {
-			person1object[i] = Math.round((income * i) / 100)
-		}
-	} else {
-		error.textContent = 'Please, fill in all fields correctly!'
-	}
-}
-
-const getPercentage2 = income => {
-	if (personTwoIncome.value != 0) {
-		for (let i = 0; i <= 100; i++) {
-			person2object[i] = Math.round((income * i) / 100)
+			personArray[i] = Math.round((income * i) / 100)
 		}
 	} else {
 		error.textContent = 'Please, fill in all fields correctly!'
@@ -47,20 +35,17 @@ const getPercentage2 = income => {
 
 const getFairShare = () => {
 	if (
-		expensesInput.value != 0 &&
+		expensesInput.valueAsNumber != 0 &&
 		expensesInput.valueAsNumber <= personOneIncome.valueAsNumber + personTwoIncome.valueAsNumber
 	) {
-		for (const [i] of Object.entries(person1object) && Object.entries(person2object)) {
+		for (let i = 0; i < personOneArray.length; i++) {
 			if (
-				person1object[i] + person2object[i] >= expensesInput.value &&
-				(person1object[i] * 100) / personOneIncome.value === (person2object[i] * 100) / personTwoIncome.value
+				personOneArray[i] + personTwoArray[i] >= expensesInput.value &&
+				(personOneArray[i] * 100) / personOneIncome.value === (personTwoArray[i] * 100) / personTwoIncome.value
 			) {
-				console.log((person1object[i] * 100) / personOneIncome.value, (person2object[i] * 100) / personTwoIncome.value)
-				console.log(person1object[i])
-				console.log(person2object[i])
-				personOneResult.textContent = person1object[i]
-				personTwoResult.textContent = person2object[i]
-				return (result.textContent = `${(person1object[i] * 100) / personOneIncome.value}%`)
+				personOneResult.textContent = personOneArray[i]
+				personTwoResult.textContent = personTwoArray[i]
+				return (result.textContent = `${(personOneArray[i] * 100) / personOneIncome.value}%`)
 			}
 		}
 	} else {
@@ -70,14 +55,17 @@ const getFairShare = () => {
 
 const allFunctions = () => {
 	getNames()
-	getPercentage1(personOneIncome.value)
-	getPercentage2(personTwoIncome.value)
+	getPercentage(personOneIncome.value, personOneArray)
+	getPercentage(personTwoIncome.value, personTwoArray)
 	getFairShare()
 }
 
-btn.addEventListener('click', allFunctions)
-document.addEventListener('keyup', e => {
-	if (e.key === 'Enter') {
-		allFunctions()
-	}
-})
+const allListeners = () => {
+	btn.addEventListener('click', allFunctions)
+	document.addEventListener('keyup', e => {
+		if (e.key === 'Enter') {
+			allFunctions()
+		}
+	})
+}
+allListeners()
